139777c2a8ddfbc9868ef35c05ea8fdff7a2fd2e
suntorch <archie.amarsanaa@gmail.com>
2024-09-24 22:43:03 +0800

    DataMember is implemented

--- Dazzler/Readers/QueryGenericReader.cs
+++ Dazzler/Readers/QueryGenericReader.cs
@@ -2,61 +2,66 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Reflection;
+using System.Runtime.Serialization;
 using Dazzler.Models;
 
 namespace Dazzler.Readers
 {
-   /// <summary>
-   /// This reader reads a records from IDbCommand and 
-   /// maps a data to the T template object which can be
-   /// Typed-Class or Anonymous class object.
-   /// </summary>
-   /// <typeparam name="T"></typeparam>
-   public class QueryGenericReader<T>
-   {
-      public List<T> Read(IDbCommand command, CommandArgs args)
-      {
-         List<T> result = new List<T>();
+    /// <summary>
+    /// This reader reads a records from IDbCommand and 
+    /// maps a data to the T template object which can be
+    /// Typed-Class or Anonymous class object.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QueryGenericReader<T>
+    {
+        public List<T> Read(IDbCommand command, CommandArgs args)
+        {
+            List<T> result = new List<T>();
 
-         using (IDataReader reader = command.ExecuteReader())
-         {
-            Dictionary<string, int> lookupIndex = new Dictionary<string, int>();
-            for (int i = 0; i < reader.FieldCount; i++) lookupIndex[reader.GetName(i).ToLower()] = i;
-
-            // pagination
-            int offset = args.Offset ?? 0;
-            int limit = args.Limit ?? int.MaxValue;
-
-            while (reader.Read())
+            using (IDataReader reader = command.ExecuteReader())
             {
-               if (offset > 0) { offset--; continue; }
+                Dictionary<string, int> lookupIndex = new Dictionary<string, int>();
+                for (int i = 0; i < reader.FieldCount; i++) lookupIndex[reader.GetName(i).ToLower()] = i;
 
-               T item = Activator.CreateInstance<T>();
-               foreach (PropertyInfo pi in typeof(T).GetProperties())
-               {
-                  // verifies whether the property name matches column name.
-                  bool success = lookupIndex.TryGetValue(pi.Name.ToLower(), out int index);
-                  if (!success) continue;
+                // pagination
+                int offset = args.Offset ?? 0;
+                int limit = args.Limit ?? int.MaxValue;
 
-                  // ignores if the property has [IgnoreFetch]
-                  var ignore = pi.GetCustomAttribute<IgnoreFetchAttribute>();
-                  if (ignore != null) continue;
+                while (reader.Read())
+                {
+                    if (offset > 0) { offset--; continue; }
 
-                  // reads column value.
-                  object value = reader.GetValue(index);
-                  if (value == DBNull.Value) continue; // 2019-08-15: issue with Guid
+                    T item = Activator.CreateInstance<T>();
+                    foreach (PropertyInfo pi in typeof(T).GetProperties())
+                    {
+                        // gets a DataMember name.
+                        var member = pi.GetCustomAttribute<DataMemberAttribute>();
+                        var fieldName = member?.Name ?? pi.Name;
 
-                  // assign the database value to the property.
-                  pi.SetValue(item, Utility.To(pi.PropertyType, value));
-               }
+                        // verifies whether the property name matches column name.
+                        bool success = lookupIndex.TryGetValue(fieldName.ToLower(), out int index);
+                        if (!success) continue;
 
-               result.Add(item);
-               if (--limit <= 0) break;
+                        // ignores if the property has [IgnoreFetch]
+                        var ignore = pi.GetCustomAttribute<IgnoreFetchAttribute>();
+                        if (ignore != null) continue;
 
+                        // reads column value.
+                        object value = reader.GetValue(index);
+                        if (value == DBNull.Value) continue; // 2019-08-15: issue with Guid
+
+                        // assign the database value to the property.
+                        pi.SetValue(item, Utility.To(pi.PropertyType, value));
+                    }
+
+                    result.Add(item);
+                    if (--limit <= 0) break;
+
+                }
             }
-         }
 
-         return result;
-      }
-   }
+            return result;
+        }
+    }
 }
